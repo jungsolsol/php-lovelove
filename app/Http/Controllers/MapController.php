5699612ffb2342cda04bfd419ddf547f0bf2ce73
jungsolsol <jsol78@naver.com>
2023-03-13 08:32:58 +0900

    [java - php porting] - add MAP API

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MapController extends Controller
+{
+    /**
+     * LAT, LON 데이터를 받아서
+     * members 테이블에 위치정보 업데이트 이후
+     * 주변 위치의 이성 탐색 -> index()
+     */
+
+
+    public function index(Request $request) {
+
+
+    }
+}
